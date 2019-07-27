0067439a9e7747842b3ea725acaa23a6a8a66319
Madao-3 <chrishyman256@gmail.com>
2019-07-27 14:52:10 +0800

    trying update bas64 support

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate base64;
+extern crate tempfile;
 extern crate lopdf;
 
 use wasm_bindgen::prelude::*;
@@ -7,8 +8,9 @@ use web_sys::console;
 use base64::{encode, decode};
 use lopdf::content::Content;
 use lopdf::{Document, Object, ObjectId};
+use tempfile::tempfile;
 use std::fs::File;
-use std::io::{Write, Read, Seek, SeekFrom};
+use std::io::{Write, Read, BufReader, Cursor};
 
 // ¯\_(ツ)_/¯
 const SPACE_THRESHOLD: i64 = 100;
@@ -26,8 +28,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen]
 pub fn pdf_check(base64_str: String) -> String {
-  let buf = &decode(&base64_str).unwrap()[..];
-  let document = Document::load_from(buf).unwrap();
-  let pages = document.get_pages();
-  document.version
+  let mut buffer = Vec::<u8>::new();
+  base64::decode_config_buf(&base64_str, base64::STANDARD, &mut buffer).unwrap();
+  format!("{:?}", buf)
+  let doc = Document::load_from(buffer.as_slice()).unwrap();
+  doc.version
 }
