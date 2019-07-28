1ca7edb9db0abdb7457932e1ffb41ead159f4e8c
Madao <madao@MadaodeMacBook-Pro.local>
2019-07-28 23:32:36 +0800

    add pdfloader

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,13 @@ extern crate base64;
 extern crate tempfile;
 extern crate lopdf;
 
+mod PdfLoader;
+
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 use base64::{encode, decode};
-use lopdf::content::Content;
-use lopdf::{Document, Object, ObjectId};
-use tempfile::tempfile;
-use std::fs::File;
-use std::io::{Write, Read, BufReader, Cursor};
-
-// ¯\_(ツ)_/¯
-const SPACE_THRESHOLD: i64 = 100;
-
-
+use PdfLoader::pdf::Collector;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -25,12 +18,9 @@ const SPACE_THRESHOLD: i64 = 100;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen]
 pub fn pdf_check(base64_str: String) -> String {
   let mut buffer = Vec::<u8>::new();
   base64::decode_config_buf(&base64_str, base64::STANDARD, &mut buffer).unwrap();
-  format!("{:?}", buf)
-  let doc = Document::load_from(buffer.as_slice()).unwrap();
-  doc.version
+  Collector::process_document(buffer.as_slice())
 }
